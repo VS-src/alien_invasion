836c89f00fc6dd5ce577dbd4b1eda0b6773ddcd7
= <=>
2025-09-16 22:27:40 +0300

    add movement for the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,18 +28,47 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # what keyboar and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # redraw the screen during each pass in the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
+
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_event(event)
+
+
+    def _check_keydown_events(self, event):
+        """respond to keypresses."""
+        if event.key ==pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+
+    def _check_keyup_event(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the scrren and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     # Make a game instance and run the game.
--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings:
         self.screen_width = 1600
         self.screen_height = 1200
         self.bg_color = (230,230,230)
+
+        # ship settings
+        self.ship_speed = 1.5
