3469905a758d23dab57cc47568fd046315c6fbcd
= <=>
2025-09-17 21:15:06 +0300

    add fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,9 +80,29 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Make the fleet of aliens"""
+        # Create an alien and keep adding until there is no room left.
+        # Spacing between aliens is one alien width and one alien heigh
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_heigh = alien.rect.size
 
+        current_x, current_y = alien_width, alien_heigh
+        while current_y < (self.settings.screen_height - 3 * alien_heigh):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            
+            # finished a row, reset x value and increment y value
+            current_x = alien_width
+            current_y += 2 * alien_heigh
+
+
+    def _create_alien(self, x_position, y_position):
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+           
 
     def _update_screen(self):
         """Update images on the scrren and flip to the new screen"""
