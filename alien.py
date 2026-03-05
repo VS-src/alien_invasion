541e61421def6fa10b7ef5c7120953077d972c9a
VS-src <v.silobritas@outlook.com>
2026-03-05 20:56:13 +0200

    Update alien.py

--- alien.py
+++ alien.py
@@ -4,6 +4,8 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """ A class to represent a single alien in the fleet"""
 
+
+    
     def __init__(self, ai_game):
         """Initialize the alien  and set its starting position."""
         super().__init__()
